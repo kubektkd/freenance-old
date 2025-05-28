4ea62daac7c0b1a60280911d513d5e29a11eff87
MICHNIEWICZ Jakub <jamichniewic@lyreco.com>
2025-05-28 12:14:24 +0200

    update configuration to fix frontend part

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
   server: {
     port: 5173,
     proxy: {
